47805e4716627ee3d41cb7950c5e94d0c5e04aea
Marc Dekeyser <marc.dekeyser@Hotmail.com>
2018-10-03 12:26:15 +0200

    Update Invoke-ExchangeVersionNumbersPull.ps1
    Changed cleanup code to using regex expressions, turns out more reliable that way.

--- Invoke-ExchangeVersionNumbersPull.ps1
+++ Invoke-ExchangeVersionNumbersPull.ps1
@@ -1,23 +1,25 @@
-    $WebResponse = Invoke-WebRequest "https://technet.microsoft.com/en-us/library/hh135098(v=exchg.150).aspx"
+      $WebResponse = Invoke-WebRequest "https://technet.microsoft.com/en-us/library/hh135098(v=exchg.150).aspx"
     $data = $WebResponse.AllElements | where{$_.tagname -eq "table"}
     $results = @()
 
     foreach($i in $data){
-        $Sections = (($i.innerHTML -split "<TR>")) 
-        $sections = ($sections -replace ' data-th="&#10;         Product name&#10;        ">',"" -replace ' data-th="&#10;         Release date&#10;        ">',"" -replace ' data-th="&#10;         Build number&#10;        ">',"" -replace '<TD','' -replace '<TBODY>','' -replace '</TBODY>','' -replace '</TD>','' -replace '</TR>','' -replace '<TR Responsive="true">','' -replace '<TH scope=col>Product name </TH>','' -replace '<TH scope=col>Product name </TH>','' -replace '<TH scope=col>Release date </TH>','' -replace '<TH scope=col>Build number </TH>','' -replace '</P>','' -replace '<a[^>]+href=\"(.*?)\"[^>]*>',""  -replace '</A>','' -replace 'data-th="&#10;           Release date&#10;          ">','') -replace 'data-th="&#10;             Release date&#10;            ">',''
-        foreach($section in $sections){
+        $Sections = (($i.innerHTML -split "<TR>"))
         
-            $cleanedUp = ($Section -split "<P>" ) -replace "`n",""
+        foreach($section in $sections){          
+            $ReleaseDate = ((($section -split "<P>") -split "</p>") | Select-String -Pattern "(January|February|March|April|May|June|July|August|September|October|November|December)\s\d{1,2},\s\d{4}|(January|February|March|April|May|June|July|August|September|October|November|December),\s\d{4}|(January|February|March|April|May|June|July|August|September|October|November|December)\s\d{4}|(January|February|March|April|May|June|July|August|September|October|November|December)\d{1,2},\s\d{4}") -replace "`n",""        
+            $productName = ((($section -split "<P>") -split "</p>") | Select-string -Pattern "(Exchange)") -replace "`n",""  -replace '<a[^>]+href=\"(.*?)\"[^>]*>',""  -replace '</A>',''
+            $VersionNumber = ((($section -split "<P>") -split "</p>") | select-string -Pattern "\d{1,2}\.\d{1,2}\.\d") -replace "`n",""
 
-            $results += New-Object -Type PSObject -Property (
-                @{
-                    "productName" = ($cleanedUp[1])
-                    "ReleaseDate" = ($cleanedUp[2])
-                    "VersionNumber" = ($cleanedUp[3])
-                }
-            )
+            if(!([STRING]::IsNullOrEmpty($productName))){
+                $results += New-Object -Type PSObject -Property (
+                    @{
+                        "productName" = ($ReleaseDate)
+                        "ReleaseDate" = ($productName)
+                        "VersionNumber" = ($VersionNumber)
+                    }
+                )
+            }
         }
-    }
-    
-    return $results
+    } 
 
+    return $results
